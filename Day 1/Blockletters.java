23acddec8f306494cb3f91d7b771a805366413a6
b2tgeorge <bttgeorge@gmail.com>
2026-08-28 14:42:48 -0700

    Day 1- Printing

---
+++ Day 1/Blockletters.java
@@ -0,0 +1,8 @@
+public class Blockletters {
+    public static void main(String[] args) {
+        System.out.println("B B B B B\nB        B\nB        B\nB B B B B\nB        B\nB        B\nB B B B B");
+        System.out.println("\n\n  G G G G G\n G         \nG    G G G\nG        G\nG        G\nG        G\n  G G G G");
+
+        
+    }
+}
