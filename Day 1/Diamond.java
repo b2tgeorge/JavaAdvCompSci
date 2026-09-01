f593e79af65ddf9276127d33a4b83354dd2ec6e0
b2tgeorge <bttgeorge@gmail.com>
2026-09-01 12:50:01 -0700

    Day 2 Variables

--- Day 1/Diamond.java
+++ Day 1/Diamond.java
@@ -1,5 +1,5 @@
 
-public class Printing {
+public class Diamond {
     public static void main(String[] args) {
         System.out.println("*\n***\n*****\n*******\n*********\n*******\n*****\n***\n*");
         
